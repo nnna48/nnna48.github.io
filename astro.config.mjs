f1443c2a72d64b383053a2e0d535ab8bbdaab2a9
xiaye <xiaye@msn.com>
2026-08-19 22:04:13 +0800

    feat: 添加动态页评论逻辑和更新 Layout 组件的规范链接

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,6 +32,8 @@ import {
 	fontsList,
 	mermaidConfig,
 	plantumlConfig,
+	commentConfig,
+	dynamicConfig,
 	siteConfig,
 } from "./src/config";
 import I18nKey from "./src/i18n/i18nKey";
@@ -258,7 +260,16 @@ export default defineConfig({
 				if (pathname === "/vndb/" && !siteConfig.pages.vndb) {
 					return false;
 				}
-				if (pathname === "/mal/" && !siteConfig.pages.mal) {
+				if (pathname === "/myanimelist/" && !siteConfig.pages.mal) {
+					return false;
+				}
+				// 动态页评论嵌入页：评论关闭时重定向到 /404/，不应进 sitemap
+				if (
+					pathname === "/dynamic/comments/" &&
+					(dynamicConfig.showComment === false ||
+						!commentConfig.type ||
+						commentConfig.type === "none")
+				) {
 					return false;
 				}
 				if (pathname === "/sponsor/" && !siteConfig.pages.sponsor) {
