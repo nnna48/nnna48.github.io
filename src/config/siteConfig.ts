1d186588478933e6068c432240e2bf6a2abc8cee
nnna48 <2087879025@qq.com>
2026-08-25 21:13:47 +0800

    Configure Firefly blog

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -46,7 +46,7 @@ export const siteConfig: SiteConfig = {
 	subtitle: "Demo site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://nnna48.github.io",
 
 	// 站点描述
 	description:
